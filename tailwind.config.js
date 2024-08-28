e1d9f4c776156205d82f239f9e1f01f7b4ac1567
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-08-28 16:56:56 -0400

    re add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +19,30 @@ module.exports = {
     },
     extend: {
       colors: {
+        correct: {
+          light: "#6aaa64",
+          dark: "#538d4e",
+          hc: "#f5793a",
+          hc_dark: "#f5793a",
+        },
+        absent: {
+          light: "#787c7e",
+          dark: "#3a3a3c",
+          hc: "#787c7e",
+          hc_dark: "#3a3a3c",
+        },
+        present: {
+          light: "#c9b458",
+          dark: "#b59f3b",
+          hc: "#85c0f9",
+          hc_dark: "#85c0f9",
+        },
+        keyboard: {
+          light: "#d3d6da",
+          dark: "#818384",
+          hc: "#d3d6da",
+          hc_dark: "#818384",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -73,5 +98,12 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [
+    require("tailwindcss-animate"),
+    plugin(function ({ addVariant }) {
+      addVariant("high-contrast", "&:is(.high-contrast *)");
+      addVariant("high-contrast-dark", "&:is(.high-contrast-dark *)");
+      addVariant("light", "&:is(.light *)");
+    }),
+  ],
+};
